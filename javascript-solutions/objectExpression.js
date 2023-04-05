18ee3d47429d14b07a2c457ff093b004f2b5f6b0
Dmitry Krasnoyarov <368378@niuitmo.ru>
2023-04-05 22:26:09 +0300

    hw7 base + ExpLn refactored

--- javascript-solutions/objectExpression.js
+++ javascript-solutions/objectExpression.js
@@ -2,120 +2,114 @@
 
 function Const(value) {
     this.value = value
-    this.evaluate = function (x, y, z) {
+}
+
+Const.prototype = {
+    evaluate: function () {
         return this.value
-    }
-    this.toString = function () {
+    }, toString: function () {
         return this.value.toString()
     }
 }
 
 function Variable(name) {
     this.name = name
-    this.evaluate = function (x, y, z) {
+}
+
+Variable.prototype = {
+    evaluate: function (x, y, z) {
         return {x, y, z}[this.name]
-    }
-    this.toString = function () {
+    }, toString: function () {
         return this.name
     }
 }
 
-function BinaryOperation(left, right, operation, sign) {
-    this.left = left
-    this.right = right
-    this.operation = operation
-    this.sign = sign
-}
-
-BinaryOperation.prototype = {
-    evaluate: function (x, y, z) {
-        return this.operation(this.left.evaluate(x, y, z), this.right.evaluate(x, y, z));
-    },
-    toString: function () {
-        return `${this.left.toString()} ${this.right.toString()} ${this.sign}`;
-    },
-};
-
-function Add(left, right) {
-    BinaryOperation.call(this, left, right, (a, b) => a + b, '+');
-}
-
-Add.prototype = BinaryOperation.prototype;
-
-function Subtract(left, right) {
-    BinaryOperation.call(this, left, right, (a, b) => a - b, '-');
-}
-
-Subtract.prototype = BinaryOperation.prototype;
-
-function Multiply(left, right) {
-    BinaryOperation.call(this, left, right, (a, b) => a * b, '*');
-}
-
-Multiply.prototype = BinaryOperation.prototype;
-
-function Divide(left, right) {
-    BinaryOperation.call(this, left, right, (a, b) => a / b, '/');
-}
-
-Divide.prototype = BinaryOperation.prototype;
-
-function UnaryOperation(value, operation, sign) {
-    this.value = value;
+function Operation(operation, sign, ...args) {
+    this.args = args;
     this.operation = operation;
     this.sign = sign;
 }
 
-UnaryOperation.prototype = {
-    evaluate: function (x, y, z) {
-        return this.operation(this.value.evaluate(x, y, z));
-    },
-    toString: function () {
-        return `${this.value.toString()} ${this.sign}`;
-    },
+Operation.prototype = {
+    evaluate: function (...args) {
+        return this.operation(...this.args.map(arg => arg.evaluate(...args)));
+    }, toString: function () {
+        return `${this.args.join(' ')} ${this.sign}`;
+    }
 }
 
-function Negate(value) {
-    UnaryOperation.call(this, value, a => -a, 'negate')
+function Add(...args) {
+    Operation.call(this, (a, b) => a + b, '+', ...args);
 }
 
-Negate.prototype = UnaryOperation.prototype;
+Add.prototype = Operation.prototype;
 
-function Exp(value) {
-    UnaryOperation.call(this, value, a => Math.exp(a), 'exp')
+function Subtract(...args) {
+    Operation.call(this, (a, b) => a - b, '-', ...args);
 }
 
-Exp.prototype = UnaryOperation.prototype;
+Subtract.prototype = Operation.prototype;
 
-function Ln(value) {
-    UnaryOperation.call(this, value, a => Math.log(a), 'ln')
+function Multiply(...args) {
+    Operation.call(this, (a, b) => a * b, '*', ...args);
 }
 
-Ln.prototype = UnaryOperation.prototype;
+Multiply.prototype = Operation.prototype;
+
+function Divide(...args) {
+    Operation.call(this, (a, b) => a / b, '/', ...args);
+}
+
+Divide.prototype = Operation.prototype;
+
+function Negate(...args) {
+    Operation.call(this, a => -a, 'negate', ...args);
+}
+
+Negate.prototype = Operation.prototype;
+
+function Exp(...args) {
+    Operation.call(this, a => Math.exp(a), 'exp', ...args);
+}
+
+Exp.prototype = Operation.prototype;
+
+function Ln(...args) {
+    Operation.call(this, a => Math.log(a), 'ln', ...args);
+}
+
+Ln.prototype = Operation.prototype;
+
+const map = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => a / b,
+    'negate': a => -a,
+    'exp': a => Math.exp(a),
+    'ln': a => Math.log(a),
+
+}
 
 function parse(expression) {
     const stack = [];
-    const tokens = expression.split(' ').filter(token => token.trim() !== '');
-
-    const operations = {
-        '+': Add,
-        '-': Subtract,
-        '*': Multiply,
-        '/': Divide,
-        'negate': Negate,
-        'exp': Exp,
-        'ln': Ln,
-    };
-    for (const token of tokens) {
-        if (token === 'x' || token === 'y' || token === 'z') {
-            stack.push(new Variable(token));
-        } else if (token in operations) {
-            const amountOfArgsOfClass = operations[token].length;
-            const args = stack.splice(-amountOfArgsOfClass);
-            stack.push(new operations[token](...args));
+    const tokens = expression.split(" ");
+    for (let token of tokens) {
+        if (token === '') {
+            continue;
+        }
+        if (token in map) {
+            const args = []
+            for (let i = 0; i < map[token].length; i++) {
+                args.push(stack.pop())
+            }
+            stack.push(new Operation(map[token], token, ...args.reverse()))
+        } else if (token in {'x': 0, 'y': 1, 'z': 2}) {
+            stack.push(new Variable(token))
         } else {
-            stack.push(new Const(parseFloat(token)));
+            stack.push(new Const(+token))
         }
     }
-    return stack[0];
+    return stack.pop();
 }
+

256b6d9cd65f09608f77bf2b4be4a3c6fd05d1c9
Dmitry Krasnoyarov <368378@niuitmo.ru>
2023-05-16 17:49:51 +0300

    hw 10 + ExpLn modification added

--- clojure-solutions/expression.clj
+++ clojure-solutions/expression.clj
@@ -42,4 +42,60 @@
                          parsedOperands (map #(parseFunction (str %)) operandList)]
                         (apply operatorFunction parsedOperands)))))
 
+(load-file "proto.clj")
 
+(def evaluate (method :evaluate))
+(def toString (method :toString))
+
+(def operands (field :operands))
+(def getValue (field :value))
+(def getOp (field :op))
+(def getStringOp (field :stringOp))
+
+(defn Constant [number] {
+                         :value    number
+                         :evaluate (fn [expression varsAndValues] (getValue expression))
+                         :toString (fn [expression] (str (getValue expression)))})
+
+
+(defn Variable [name] {
+                       :value    name
+                       :evaluate (fn [expression varsAndValues] (varsAndValues (getValue expression)))
+                       :toString (fn [expression] (getValue expression))})
+
+(defn Operation [op stringOp & args]
+      {
+       :op       op
+       :stringOp stringOp
+       :operands args
+       :evaluate (fn [expression varsAndValues]
+                     (apply op (mapv #(evaluate % varsAndValues) (operands expression))))
+       :toString (fn [expression]
+                     (str "(" stringOp " " (clojure.string/join " " (mapv toString (operands expression))) ")"))})
+
+(defn Add [a b] (Operation + "+" a b))
+(defn Subtract [a b] (Operation - "-" a b))
+(defn Multiply [a b] (Operation * "*" a b))
+(defn Divide [a b] (Operation correctDivision "/" a b))
+(defn Negate [a] (Operation - "negate" a))
+
+(defn Sin [a] (Operation #(Math/sin %) "sin" a))
+(defn Cos [a] (Operation #(Math/cos %) "cos" a))
+
+(defn parseObject [rawExpression]
+      (let [expression (read-string rawExpression)]
+           (cond
+             (number? expression) (Constant expression)
+             (symbol? expression) (Variable (str expression))
+             :else (let [operatorSymbol (first expression)
+                         operandList (rest expression)
+                         operatorFunction (case operatorSymbol
+                                                + Add
+                                                - Subtract
+                                                * Multiply
+                                                / Divide
+                                                negate Negate
+                                                sin Sin
+                                                cos Cos)
+                         parsedOperands (map #(parseObject (str %)) operandList)]
+                        (apply operatorFunction parsedOperands)))))
